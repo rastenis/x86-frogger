81a68527b014a06f76d21c02ae092642e517347e
Matas Rastenis <matas2k@gmail.com>
2019-10-21 20:49:58 +0200

    moved out menu

--- 7/src/game/game.s
+++ 7/src/game/game.s
@@ -5,59 +5,27 @@
 
 .section .game.data
 
-string1: .asciz "Frogger v0.0.1"
-string2: .asciz "      By:"
-string3: .asciz "xx and xx"
-string4: .asciz "1) Start game"
-
-
-format: .asciz "%s"
-
-
 .section .game.text
 
 gameInit:
 	# call generation for the first time
+	movq	$0,	%rbx
 	ret
 
+
 gameLoop:
+	# 
+	# registers used:
+	# 	%rbx   program stage counter; 0 is the initial value (menu stage), 1 is the game stage, 2 is high score, 3 is exit.
+	# 	...
+	#
 
-	pushq   %rbp                # store the old base pointer
-	movq    %rsp, %rbp          # store current stack pointer as base pointer
+	# MENU STAGE:
+	_menu: 
+	call showMenu
 
-	# Print the title
-	movq    $32, %rsi           # x = 32
-	movq    $7, %rdx            # y = 7
-	movq    $0x0f, %rcx         # black background, white foreground
-	movq   	$string1, %r8
-	movq    $format, %rdi
-	call    printf_coords
-	
-    # Print the menu options
-	movq    $32, %rsi           # x = 5
-	movq    $8, %rdx            # y = 5
-	movq    $0x0f, %rcx         # black background, white foreground
-	movq   	$string2, %r8
-	movq    $format, %rdi
-	call    printf_coords
-	# Prin
-	movq    $32, %rsi           # x = 5
-	movq    $9, %rdx            # y = 5
-	movq    $0x0f, %rcx         # black background, white foreground
-	movq   	$string3, %r8
-	movq    $format, %rdi
-	call    printf_coords
-	# Prin
-	movq    $32, %rsi           # x = 5
-	movq    $15, %rdx            # y = 5
-	movq    $0x0f, %rcx         # black background, white foreground
-	movq   	$string4, %r8
-	movq    $format, %rdi
-	call    printf_coords
-
-	# LEFTOFF: loop until enter or H or Q pressed (or esc) move this to menu.s
-
-	movq    %rbp, %rsp          # discard local variables
-	popq    %rbp                # restore the base pointer
+	# GAME STAGE
+	_game_loop:
+	# ... call game loop
 
 	ret
---
+++ 7/src/game/menu.s
@@ -0,0 +1,57 @@
+.file "src/game/game.s"
+
+.global showMenu
+
+.section .game.data
+
+string1: .asciz "Frogger v0.0.1"
+string2: .asciz "      By:"
+string3: .asciz "xx and xx"
+string4: .asciz "1) Start game"
+
+format: .asciz "%s"
+
+.section .game.text
+
+showMenu:
+
+	pushq   %rbp                # store the old base pointer
+	movq    %rsp, %rbp          # store current stack pointer as base pointer
+
+	# Print the title
+	movq    $32, %rsi           # x = 32
+	movq    $7, %rdx            # y = 7
+	movq    $0x0f, %rcx         # black background, white foreground
+	movq   	$string1, %r8
+	movq    $format, %rdi
+	call    printf_coords
+	
+    # Print the menu options
+	movq    $32, %rsi           # x = 5
+	movq    $8, %rdx            # y = 5
+	movq    $0x0f, %rcx         # black background, white foreground
+	movq   	$string2, %r8
+	movq    $format, %rdi
+	call    printf_coords
+	# Prin
+	movq    $32, %rsi           # x = 5
+	movq    $9, %rdx            # y = 5
+	movq    $0x0f, %rcx         # black background, white foreground
+	movq   	$string3, %r8
+	movq    $format, %rdi
+	call    printf_coords
+	# Prin
+	movq    $32, %rsi           # x = 5
+	movq    $15, %rdx            # y = 5
+	movq    $0x0f, %rcx         # black background, white foreground
+	movq   	$string4, %r8
+	movq    $format, %rdi
+	call    printf_coords
+
+	# LEFTOFF: loop until enter or H or Q pressed (or esc) move this to menu.s
+
+	movq    %rbp, %rsp          # discard local variables
+	popq    %rbp                # restore the base pointer
+
+	ret
+
